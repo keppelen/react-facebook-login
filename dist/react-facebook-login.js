6b591561dfa4faed383addb9692a18425be60ba8
Giovanni Keppelen <keppelen.as@gmail.com>
2015-04-22 16:32:07 -0300

    fix

--- dist/react-facebook-login.js
+++ dist/react-facebook-login.js
@@ -2,7 +2,7 @@ var FacebookLogin = React.createClass({displayName: "FacebookLogin",
   render: function() {
     return (
       React.createElement("div", null, 
-        React.createElement("button", {className: this.props.class, onClick: this.handleClick}, this.props.callToActiion), 
+        React.createElement("button", {className: this.props.class, onClick: this.handleClick}, this.props.calltoaction), 
         React.createElement("div", {id: "fb-root"})
       )
     )
