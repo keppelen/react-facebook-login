f6539d5c8477bf0ef6a3112206cfc122027dc1c6
Julien Bouquillon <julien@revolunet.com>
2016-09-07 11:42:46 +0200

    fix initial login state

--- src/facebook.js
+++ src/facebook.js
@@ -54,7 +54,7 @@ class FacebookLogin extends React.Component {
       });
 
       if (autoLoad || window.location.search.includes('facebookdirect')) {
-        window.FB.getLoginStatus(this.checkLoginState);
+         window.FB.getLoginStatus(this.checkLoginAfterRefresh);
       }
     };
     // Load the SDK asynchronously
@@ -83,6 +83,14 @@ class FacebookLogin extends React.Component {
       if (this.props.callback) {
         this.props.callback({ status: response.status });
       }
+    }
+  };
+
+  checkLoginAfterRefresh = (response) => {
+    if (response.status === "unknown") {
+      window.FB.login(loginResponse => this.checkLoginState(loginResponse), true)
+    } else {
+      this.checkLoginState(response);
     }
   };
 
