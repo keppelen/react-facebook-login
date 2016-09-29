dbd0bb5b5fef98d7daad68575b54fe781f87865d
keppelen <keppelen.as@gmail.com>
2016-09-29 11:17:46 -0300

    v3.3.0

--- src/objectToParams.js
+++ src/objectToParams.js
@@ -2,7 +2,7 @@
  * Encode object to url parameters
  *
  * @param      {Object} paramsObj The object needs to encode as url parameters
- * @return     {String} Encoded url parameters 
+ * @return     {String} Encoded url parameters
  */
 export default paramsObj => {
   let str = '';
