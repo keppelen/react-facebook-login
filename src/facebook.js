aa569848b38dd1a653e540f047888170e2ada1c1
keppelen <keppelen.as@gmail.com>
2016-10-17 14:26:54 -0200

    fix lint

--- src/facebook.js
+++ src/facebook.js
@@ -58,7 +58,7 @@ class FacebookLogin extends React.Component {
       });
 
       if (autoLoad || window.location.search.includes('facebookdirect')) {
-         window.FB.getLoginStatus(this.checkLoginAfterRefresh);
+        window.FB.getLoginStatus(this.checkLoginAfterRefresh);
       }
     };
     // Load the SDK asynchronously
@@ -91,8 +91,8 @@ class FacebookLogin extends React.Component {
   };
 
   checkLoginAfterRefresh = (response) => {
-    if (response.status === "unknown") {
-      window.FB.login(loginResponse => this.checkLoginState(loginResponse), true)
+    if (response.status === 'unknown') {
+      window.FB.login(loginResponse => this.checkLoginState(loginResponse), true);
     } else {
       this.checkLoginState(response);
     }
