59a525e3dff39a0d58508e9dd960bc948f0e4b99
Adam Goldman <adamgoldman7@gmail.com>
2016-11-21 15:37:04 +0200

    Protect unwanted clicks  (#76)

--- src/facebook.js
+++ src/facebook.js
@@ -6,6 +6,7 @@ import objectToParams from './objectToParams';
 class FacebookLogin extends React.Component {
 
   static propTypes = {
+    isDisabled: PropTypes.bool,
     callback: PropTypes.func.isRequired,
     appId: PropTypes.string.isRequired,
     xfbml: PropTypes.bool,
@@ -38,6 +39,11 @@ class FacebookLogin extends React.Component {
     language: 'en_US',
   };
 
+  state = {
+    isLoaded: false,
+    isProcessing: false,
+  };
+
   componentDidMount() {
     const { appId, xfbml, cookie, version, autoLoad, language } = this.props;
     let fbRoot = document.getElementById('fb-root');
@@ -56,7 +62,7 @@ class FacebookLogin extends React.Component {
         xfbml,
         cookie,
       });
-
+      this.setState({ isLoaded: true });
       if (autoLoad || window.location.search.includes('facebookdirect')) {
         window.FB.getLoginStatus(this.checkLoginAfterRefresh);
       }
@@ -87,6 +93,7 @@ class FacebookLogin extends React.Component {
   };
 
   checkLoginState = (response) => {
+    this.setState({ isProcessing: false });
     if (response.authResponse) {
       this.responseApi(response.authResponse);
     } else {
@@ -105,6 +112,10 @@ class FacebookLogin extends React.Component {
   };
 
   click = () => {
+    if (!this.state.isLoaded || this.state.isProcessing || this.props.isDisabled) {
+      return;
+    }
+    this.setState({ isProcessing: true });
     const { scope, appId, onClick, reAuthenticate } = this.props;
 
     if (typeof onClick === 'function') {
@@ -145,12 +156,20 @@ class FacebookLogin extends React.Component {
     return false;
   }
 
+  // [AdGo] 20.11.2016 - coult not get container class to work
+  containerStyle() {
+    const style = { transition: 'opacity 0.5s' };
+    if (this.state.isProcessing || !this.state.isLoaded || this.props.isDisabled) {
+      style.opacity = 0.6;
+    }
+    return style;
+  }
+
   render() {
     const { cssClass, size, icon, textButton } = this.props;
     const isIconString = typeof icon === 'string';
-
     return (
-      <span>
+      <span style={ this.containerStyle() }>
         {isIconString && (
           <link
             rel="stylesheet"
