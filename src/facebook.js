12021c73a4895cc98ab1fb4c2f11bd3ee84174a9
Tung Vu <gmanvn@gmail.com>
2017-05-02 17:38:10 +0700

    make sure getIsMobile is a boolean value (#129)

--- src/facebook.js
+++ src/facebook.js
@@ -7,7 +7,7 @@ const getIsMobile = () => {
   let isMobile = false;
 
   try {
-    isMobile = ((window.navigator && window.navigator.standalone) || navigator.userAgent.match('CriOS') || navigator.userAgent.match(/mobile/i));
+    isMobile = !!((window.navigator && window.navigator.standalone) || navigator.userAgent.match('CriOS') || navigator.userAgent.match(/mobile/i));
   } catch (ex) {
     // continue regardless of error
   }
