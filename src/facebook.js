7816c5c27c8c6eb12822a154c9d11d25e56656f5
Andrew Kamphuis <andrewkamphuis@gmail.com>
2017-05-07 10:47:48 -0700

    updated to prop-types package (#132)

--- src/facebook.js
+++ src/facebook.js
@@ -1,5 +1,6 @@
 // @flow
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import styles from '../styles/facebook.scss';
 import objectToParams from './objectToParams';
 
