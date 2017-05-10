8cff213cd06fc6728c7b1b5bc37c94e8cbe894c0
Arfian Adam <arfianadam@yahoo.com>
2017-05-10 13:44:21 +0700

    bundle react into development state instead of production

--- webpack.config.dist.js
+++ webpack.config.dist.js
@@ -30,11 +30,6 @@ module.exports = {
   },
 
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env':{
-        'NODE_ENV': JSON.stringify('production'),
-      },
-    }),
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
