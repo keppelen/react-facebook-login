ebdf23bcc8bdd385e1a551632d3216fa3cc53390
kdepp <kdepp.cd@gmail.com>
2017-07-04 22:28:29 +0800

    fix: facebook sdk url from all.js to sdk.js

--- src/facebook.js
+++ src/facebook.js
@@ -132,7 +132,7 @@ class FacebookLogin extends React.Component {
       let js = element;
       if (d.getElementById(id)) { return; }
       js = d.createElement(s); js.id = id;
-      js.src = `https://connect.facebook.net/${language}/all.js`;
+      js.src = `https://connect.facebook.net/${language}/sdk.js`;
       fjs.parentNode.insertBefore(js, fjs);
     })(document, 'script', 'facebook-jssdk');
   }
