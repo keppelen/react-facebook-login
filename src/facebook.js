36a66c064e12b9c9e9b3d5cc54d8cb59679fc11a
Hein Thanth <official.heinthanth@gmail.com>
2020-05-23 13:24:20 +0630

    Replace componentWillReceiveProps with shouldComponentUpdate
    Sync with latest lifecycles

--- src/facebook.js
+++ src/facebook.js
@@ -78,7 +78,8 @@ class FacebookLogin extends React.Component {
       document.body.appendChild(fbRoot);
     }
   }
-  componentWillReceiveProps(nextProps) {
+
+  shouldComponentUpdate(nextProps, nextStates) {
     if (this.state.isSdkLoaded && nextProps.autoLoad && ! this.props.autoLoad) {
       window.FB.getLoginStatus(this.checkLoginAfterRefresh);
     }
